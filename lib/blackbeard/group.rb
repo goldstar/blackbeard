1bf48526d70a2ad4d19612d2ad5628cb65e91a97
Robert Graff <robert_graff@yahoo.com>
2014-02-15 16:45:14 -0800

    Determine segments from context

--- lib/blackbeard/group.rb
+++ lib/blackbeard/group.rb
@@ -9,8 +9,33 @@ module Blackbeard
       storable_attributes_hash[:name] || id
     end
 
+    def segment(context)
+      return nil unless definition
+      raw_segment = definition.call(context.request, context.user)
+      segment = case raw_segment
+      when false
+        nil
+      when nil
+        nil
+      when true
+        id
+      else
+        segment.to_s
+      end
+      add_segment(segment) unless segment.nil?
+      segment
+    end
+
+    def definition
+      config.group_definitions[self.id.to_sym]
+    end
+
+    def add_segment
+      # TODO
+    end
+
     def segments
-      []
+      [] # TODO
     end
 
   end
