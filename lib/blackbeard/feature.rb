0ecf120f256f569f545a03eae550dd6130beb96e
Robert Graff <robert_graff@yahoo.com>
2014-03-02 19:02:15 -0800

    Add and remove segments from features.

--- lib/blackbeard/feature.rb
+++ lib/blackbeard/feature.rb
@@ -4,19 +4,31 @@ module Blackbeard
   class Feature < Storable
     set_master_key :features
     string_attributes :name, :description, :status
+    json_attributes :group_segments
+
+    def segments_for(group_id)
+      (group_segments && group_segments[group_id.to_s]) || []
+    end
+
+    def set_segments_for(group_id, *segments)
+      segments = Array(segments).flatten.compact.map{|a| a.to_s}
+      grp_segments = self.group_segments || {}
+      grp_segments[group_id.to_s] = segments
+      self.group_segments = grp_segments
+    end
 
     def active_for?(context)
       case status
       when 'active'
         true
       when 'rollout'
-        in_active_group?(context) || active_user?(context) || active_visitor?(context)
+        in_active_segment?(context) || active_user?(context) || active_visitor?(context)
       else
         false
       end
     end
 
-    def inactive_group?(context)
+    def inactive_segment?(context)
       false
     end
 
--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -3,6 +3,20 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 describe Blackbeard::Feature do
   let(:feature){ Blackbeard::Feature.create('example') }
 
+  describe "segments_for and set_segments_for" do
+    it "should return an empty list if no segments" do
+      feature.segments_for(:nothing).should == []
+    end
+
+    it "should return the segments for the group" do
+      feature.set_segments_for(:hello, ["world", "goodbye"])
+      feature.set_segments_for(:foo, "bar")
+      feature.segments_for(:hello).should include("world","goodbye")
+      feature.segments_for(:hello).should_not include("bar")
+      feature.segments_for(:foo).should == ["bar"]
+    end
+  end
+
   describe "#active_for?" do
     let(:context){ double }
 
