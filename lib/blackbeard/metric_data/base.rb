0f89f87c38dbbe523279ef724726492889503229
Robert Graff <robert_graff@yahoo.com>
2014-03-08 14:20:14 -0800

    Small up the uid generator.

--- lib/blackbeard/metric_data/base.rb
+++ lib/blackbeard/metric_data/base.rb
@@ -45,18 +45,29 @@ module Blackbeard
 
       def key
         @key ||= begin
-          lookup_hash = "metric_data_keys"
-          lookup_field = "metric-#{metric.id}"
-          lookup_field += "::group-#{group.id}" if group
-          uid = db.hash_get(lookup_hash, lookup_field)
-          if uid.nil?
-            uid = db.increment("metric_data_next_uid")
-            # write and read to avoid race conditional writes
-            db.hash_key_set_if_not_exists(lookup_hash, lookup_field, uid)
-            uid = db.hash_get(lookup_hash, lookup_field)
-          end
           "data::#{uid}"
         end
+      end
+
+      def uid
+        db.hash_get(lookup_hash, lookup_field) || generate_uid
+      end
+
+      def lookup_hash
+        "metric_data_keys"
+      end
+
+      def lookup_field
+        lookup_field = "metric-#{metric.id}"
+        lookup_field += "::group-#{group.id}" if group
+        lookup_field
+      end
+
+      def generate_uid
+        uid = db.increment("metric_data_next_uid")
+        # write and read to avoid race conditional writes
+        db.hash_key_set_if_not_exists(lookup_hash, lookup_field, uid)
+        db.hash_get(lookup_hash, lookup_field)
       end
 
       def segments
