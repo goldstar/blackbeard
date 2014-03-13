9139543359c7c526aa1eb06884554541df1cb537
Robert Graff <robert_graff@yahoo.com>
2014-03-13 15:55:21 -0700

    juking codeclimate

--- dashboard/routes/metrics.rb
+++ dashboard/routes/metrics.rb
@@ -7,30 +7,38 @@ module Blackbeard
       end
 
       get "/metrics/:type/:type_id" do
-        @metric = Metric.find(params[:type], params[:type_id]) or pass
-        @group = Group.find(params[:group_id]) if params[:group_id]
-        @cohort = Cohort.find(params[:cohort_id]) if params[:cohort_id]
+        ensure_metric; find_group; find_cohort
         erb 'metrics/show'.to_sym
       end
 
       post "/metrics/:type/:type_id" do
-        @metric = Metric.find(params[:type], params[:type_id]) or pass
+        ensure_metric
         @metric.update_attributes(params)
         "OK"
       end
 
       post "/metrics/:type/:type_id/groups" do
-        @metric = Metric.find(params[:type], params[:type_id]) or pass
-        @group = Group.find(params[:group_id])
+        ensure_metric; find_group
         @metric.add_group(@group) if @group
         redirect url("metrics/#{@metric.type}/#{@metric.type_id}?group_id=#{@group.id}")
       end
 
       post "/metrics/:type/:type_id/cohorts" do
-        @metric = Metric.find(params[:type], params[:type_id]) or pass
-        @cohort = Cohort.find(params[:cohort_id])
+        ensure_metric; find_cohort
         @metric.add_cohort(@cohort) if @cohort
         redirect url("metrics/#{@metric.type}/#{@metric.type_id}?cohort_id=#{@cohort.id}")
+      end
+
+      def ensure_metric
+        @metric = Metric.find(params[:type], params[:type_id]) or pass
+      end
+
+      def find_group
+        @group = Group.find(params[:group_id]) if params[:group_id]
+      end
+
+      def find_cohort
+        @cohort = Cohort.find(params[:cohort_id]) if params[:cohort_id]
       end
 
     end
