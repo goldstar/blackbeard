9d334547890ec99c5cebbf6e327049b8a60b427c
Robert Graff <robert_graff@yahoo.com>
2014-03-13 16:51:37 -0700

    Remove unused method.

--- lib/blackbeard/dashboard_helpers.rb
+++ lib/blackbeard/dashboard_helpers.rb
@@ -3,14 +3,5 @@ module Blackbeard
     def url(path = '')
       env['SCRIPT_NAME'].to_s + '/' + path
     end
-
-    def metric_data(metric, group_or_cohort)
-      if group_or_cohort.nil?
-        metric.metric_data
-      else
-        group_or_cohort.metric_data(metric)
-      end
-    end
-
   end
 end
