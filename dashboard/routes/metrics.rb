4bfb162bfbc0ad37c706478ab287553063ed8bf6
Robert Graff <robert_graff@yahoo.com>
2014-03-13 11:22:44 -0700

    UI to add cohorts to metrics

--- dashboard/routes/metrics.rb
+++ dashboard/routes/metrics.rb
@@ -9,6 +9,7 @@ module Blackbeard
       get "/metrics/:type/:type_id" do
         @metric = Metric.find(params[:type], params[:type_id]) or pass
         @group = Group.find(params[:group_id]) if params[:group_id]
+        @cohort = Cohort.find(params[:cohort_id]) if params[:cohort_id]
         erb 'metrics/show'.to_sym
       end
 
@@ -23,6 +24,13 @@ module Blackbeard
         @group = Group.find(params[:group_id])
         @metric.add_group(@group) if @group
         redirect url("metrics/#{@metric.type}/#{@metric.type_id}?group_id=#{@group.id}")
+      end
+
+      post "/metrics/:type/:type_id/cohorts" do
+        @metric = Metric.find(params[:type], params[:type_id]) or pass
+        @cohort = Cohort.find(params[:cohort_id])
+        @metric.add_cohort(@cohort) if @cohort
+        redirect url("metrics/#{@metric.type}/#{@metric.type_id}?cohort_id=#{@cohort.id}")
       end
 
     end
