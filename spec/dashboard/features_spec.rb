055c648d909c0115d0d58de5731755c5415b4504
Robert Graff <robert_graff@yahoo.com>
2014-04-09 15:28:11 -0700

    Add spec for adding features via dashboard.

--- spec/dashboard/features_spec.rb
+++ spec/dashboard/features_spec.rb
@@ -48,5 +48,19 @@ module Blackbeard
         feature.reload.group_segments_for(:hello).should include("world", "goodbye")
       end
     end
+
+    describe "post /features/:id/metrics" do
+      it "should add a metric to the feature" do
+        feature = Feature.create("jostling")
+        metric = Metric.create("total", "jostling")
+        post "/features/#{feature.id}/metrics", :metric_type => metric.type, :metric_type_id => metric.type_id
+
+        last_response.should be_redirect
+        follow_redirect!
+        last_request.url.should == "http://example.org/features/#{feature.id}?metric_type=#{metric.type}&metric_type_id=#{metric.type_id}"
+        feature.has_metric?(metric).should be_true
+      end
+    end
+
   end
 end
